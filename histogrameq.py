2fd173cb86380188c2562a053386d60b1273a788
Alexander Bruy <alexander.bruy@gmail.com>
2013-04-01 16:19:27 +0400

    start working on histogram equalization filter

---
+++ histogrameq.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+#******************************************************************************
+#
+# UMD
+# ---------------------------------------------------------
+# Classification for UMD
+#
+# Copyright (C) 2013 NextGIS (info@nextgis.org)
+#
+# This source is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# A copy of the GNU General Public License is available on the World Wide Web
+# at <http://www.gnu.org/licenses/>. You can also obtain it by writing
+# to the Free Software Foundation, 51 Franklin Street, Suite 500 Boston,
+# MA 02110-1335 USA.
+#
+#******************************************************************************
+
+from PyQt4.QtCore import *
+
+from qgis.core import *
+
+class HistogramEq(QgsRasterInterface):
+  def bandCount(self):
+    return self.input().bandCount()
+
+  def dataType(self, bandNo):
+    return self.input().dataType(bandNo)
+
+  def block(self, bandNo, extent, width, height):
+    pass
